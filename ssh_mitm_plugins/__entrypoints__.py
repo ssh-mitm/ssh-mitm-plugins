0183b90979ec8527070d9945d66b093b630e5be7
JakobJBauer <private@jakobjbauer.com>
2021-05-21 08:33:45 +0200

    Added DoS attack

--- ssh_mitm_plugins/__entrypoints__.py
+++ ssh_mitm_plugins/__entrypoints__.py
@@ -2,7 +2,8 @@ entry_points = {
     'SSHBaseForwarder': [
         'scriptedshell = ssh_mitm_plugins.ssh.scriptedshell:SSHScriptedForwarder',
         'stealthshell = ssh_mitm_plugins.ssh.stealthshell:SSHStealthForwarder',
-        'injectorshell = ssh_mitm_plugins.ssh.injectorshell:SSHInjectableForwarder'
+        'injectorshell = ssh_mitm_plugins.ssh.injectorshell:SSHInjectableForwarder',
+        'puttydos = ssh_mitm_plugins.ssh.putty_dos:SSHPuttyDoSForwarder'
     ],
     'SCPBaseForwarder': [
 
