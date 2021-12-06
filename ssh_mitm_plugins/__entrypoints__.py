c8602789eeaef6a40700e9e1b9475317eed3ec28
Manfred Kaiser <manfred.kaiser@logfile.at>
2021-12-06 13:09:37 +0100

    moved socks5 and inhecttunnel forwarder from ssh-mitm

--- ssh_mitm_plugins/__entrypoints__.py
+++ ssh_mitm_plugins/__entrypoints__.py
@@ -5,13 +5,8 @@ entry_points = {
         'plugin-injectorshell = ssh_mitm_plugins.ssh.injectorshell:SSHInjectableForwarder',
         'plugin-puttydos = ssh_mitm_plugins.ssh.putty_dos:SSHPuttyDoSForwarder'
     ],
-    'SCPBaseForwarder': [
-
+    'LocalPortForwardingBaseForwarder': [
+        'plugin-inject = ssh_mitm_plugins.tunnel.injectclienttunnel:InjectableClientTunnelForwarder',
+        'plugin-socks5 = ssh_mitm_plugins.tunnel.socks5:SOCKS5TunnelForwarder'
     ],
-    'BaseSFTPServerInterface': [
-
-    ],
-    'SFTPHandlerBasePlugin': [
-
-    ]
 }
